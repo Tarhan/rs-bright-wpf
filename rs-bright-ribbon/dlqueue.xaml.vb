6fbd8bdcddebad77308ae5df3b279b749a16fb21
kazukuro0824 <kazukuro0824@gmail.com>
2013-07-11 20:03:31 +0900

    Progressbar implemented

--- rs-bright-ribbon/dlqueue.xaml.vb
+++ rs-bright-ribbon/dlqueue.xaml.vb
@@ -1,3 +1,11 @@
 ﻿Public Class dlqueue
+    Public Property MaxValue As Integer
+    Public Property Value As Integer
+    Protected Overrides Sub OnRender(drawingContext As DrawingContext)
+        MyBase.OnRender(drawingContext)
+        Dim rect As New Rect(New Point(0, 0), New Size(Me.ActualWidth * Value / MaxValue, Me.ActualHeight))
+        drawingContext.DrawRectangle(Windows.Media.Brushes.GreenYellow, Nothing, rect)
+    End Sub
+
 
 End Class
