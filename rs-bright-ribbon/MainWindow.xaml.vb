d1d3486bc4a99d3778f36b24062fdab898711653
kazukuro0824 <kazukuro0824@gmail.com>
2013-07-16 17:19:10 +0900

    50% please test

--- rs-bright-ribbon/MainWindow.xaml.vb
+++ rs-bright-ribbon/MainWindow.xaml.vb
@@ -3,12 +3,12 @@ Imports System.Text.RegularExpressions
 
 Class MainWindow
     Inherits RibbonWindow
-#Region "イベントハンドラ"
     Enum vServiceKind
         Niconico
         Niconama
         Youtube
     End Enum
+#Region "イベントハンドラ"
     Private Sub dlbutton_Click(sender As Object, e As RoutedEventArgs) Handles dlbutton.Click
         Select Case DirectCast(DirectCast(e.Source, RibbonButton).Tag, vServiceKind)
             Case vServiceKind.Youtube
@@ -16,29 +16,34 @@ Class MainWindow
             Case vServiceKind.Niconico
                 'TODO
         End Select
-
     End Sub
-
     Private Sub RibbonTextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
         If Regex.IsMatch(Urlbox.Text, "http://(www\.)?youtube\.com/watch?.*") Then
-            dlbutton.IsEnabled = True
             dlbutton.Tag = vServiceKind.Youtube
-
         ElseIf Regex.IsMatch(Urlbox.Text, "http://(www\.)?nicovideo\.jp/watch/[sn][mo]\d+") Then
-            dlbutton.IsEnabled = True
             dlbutton.Tag = vServiceKind.Niconico
         Else
             dlbutton.Tag = Nothing
             dlbutton.IsEnabled = False
+            convertgroup.Visibility = Windows.Visibility.Collapsed
+            Return
         End If
+        convertgroup.Visibility = Windows.Visibility.Visible
+        dlbutton.IsEnabled = True
     End Sub
-    Private Sub PublishReq(attribute As vServiceKind, url As String)    '情報取得要求
-        Select Case attribute
+    Private Sub MenuItem_Click(sender As Object, e As RoutedEventArgs)
+        Urlbox.Paste()
+    End Sub
+
+    Private Sub Click(sender As Object, e As RoutedEventArgs)
+        Dim ext As Object = DirectCast(e.OriginalSource, RibbonButton).Tag
+        Select Case DirectCast(DirectCast(dlbutton, RibbonButton).Tag, vServiceKind)
             Case vServiceKind.Youtube
-
+                ytdl(Urlbox.Text, CStr(ext))
             Case vServiceKind.Niconico
-
+                'TODO
         End Select
+
     End Sub
 #End Region
 #Region "ロジック"
@@ -46,8 +51,33 @@ Class MainWindow
         Dim param As UriCookiePair = downloadViaGDataapi.getDownloadParam(url)
         Dim ctrl_Inst As New dlqueue
         ctrl_Inst.SetInfo(New Uri(param.Uris(18)), getStartupPath() + "\" + Uri.EscapeDataString(param.VideoInfo.Title), "", param.cookie, "")
-        'TODO ダウンロード用のディレクトリを作成↑
+        'TODO ダウンロード用のディレクトリを作成↑共通設定みたいな
         ctrl_Inst.start()
     End Sub
+    Private Sub ytdl(url As String, ext As String)
+
+        Dim param As UriCookiePair = downloadViaGDataapi.getDownloadParam(url)
+        Dim ctrl_Inst As New dlqueue
+        Dim saveto As String = getStartupPath() + "\" + Uri.EscapeDataString(param.VideoInfo.Title)
+        'TODO ダウンロード用のディレクトリを作成↑共通設定みたいな
+        ctrl_Inst.SetInfo(New Uri(param.Uris(18)), saveto, param.VideoInfo.Title, param.cookie, "", getlinestr(ext, saveto))
+        ctrl_Inst.start()
+    End Sub
+
+    ' TODO: config.xmlの自動読み込み
+    Private Sub loadffmcfg()
+
+    End Sub
+    ' TODO: outは未実装
+    Function getlinestr(ext As String, input As String, Optional out As String = "") As String
+        Dim linestr As String = rs_loadconvertCfg.getConvertablefileKinds(False)(ext)
+        linestr = linestr.Replace("<input>", input)
+        If out = "" Then
+            linestr = linestr.Replace("<output>", IO.Path.ChangeExtension(input, ext))
+        Else
+            linestr = linestr.Replace("<output>", IO.Path.ChangeExtension(out, ext))
+        End If
+        Return linestr
+    End Function
 #End Region
 End Class
---
+++ rs-bright-ribbon/rsun_loadconvertCfg.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Xml
+Module rs_loadconvertCfg
+    Dim _xml As XmlDocument
+    Private Sub Main()
+        If chkExist() AndAlso _xml Is Nothing Then
+            _xml = New XmlDocument
+            _xml.Load(New IO.FileStream(getStartupPath() + "\config.xml", IO.FileMode.Open, IO.FileAccess.Read, IO.FileShare.Read))
+        End If
+    End Sub
+    Private Function chkExist() As Boolean
+        Return IO.File.Exists(getStartupPath() + "\config.xml")
+    End Function
+
+    Function getConvertablefileKinds(forBuildCvtList As Boolean) As Dictionary(Of String, String)
+        If _xml Is Nothing Then
+            Main()
+        End If
+        Dim l As New Dictionary(Of String, String)
+        If forBuildCvtList Then
+            For Each e As XmlNode In _xml.GetElementsByTagName("root")
+                For Each e2 As XmlNode In e.ChildNodes
+                    For Each e3 As XmlElement In e2.ChildNodes
+                        l.Add(e3.InnerText + "| *." + e3.Name, e3.GetAttribute("arg"))
+                        Debug.WriteLine(String.Format("Add an item {0}(Type {1})", e3.GetAttribute("arg"), e3.Name))
+                    Next
+                Next
+            Next
+        Else
+            For Each e As XmlElement In _xml.GetElementsByTagName("root")
+                For Each e2 As XmlElement In e.ChildNodes
+                    For Each e3 As XmlElement In e2.ChildNodes
+                        l.Add(e3.Name, e3.GetAttribute("arg"))
+                    Next
+                Next
+            Next
+        End If
+        Return l
+    End Function
+
+    Function buildCvtListForSFD() As String()
+        'この関数は、SaveFileDialog.Filter 向けのデータを返します。
+        Dim returnvalue As New List(Of String)
+        For Each x As String In getConvertablefileKinds(True).Keys
+            returnvalue.Add(x)
+        Next
+        Return returnvalue.ToArray()
+        returnvalue.Clear()
+    End Function
+End Module
